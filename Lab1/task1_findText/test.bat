30bd52b40dca9240b492e208fee5703533dc9afc
Artem Angaev <ololok@mail.ua>
2018-02-24 23:13:57 +0300

    Просто коммит
    спать хочу

--- Lab1/task1_findText/test.bat
+++ Lab1/task1_findText/test.bat
@@ -9,12 +9,20 @@ rem При запуске без параметров ожидается ненулевой код возврата
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата
-%PROGRAM% input.txt Hello
+%PROGRAM% test_data\input.txt Hello >nul
 if ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами, но без возможности прочитать файл (не сущ) ожидает ненулевой код возврата
-%PROGRAM% notExistFile.txt Hello
+%PROGRAM% notExistFile.txt Hello >nul
 if NOT ERRORLEVEL 1 goto err
+
+rem далее тесты связаные непосредственно с поиском подстроки в строке
+
+rem программа при правильных параметрах и существующем файле прочитает весь файл и выведет его в output
+rem %PROGRAM% test_data\input.txt Hello >"%temp%/\output.txt"
+rem fc.exe "%temp%\output.txt" test_data\input.txt >nul
+rem if ERRORLEVEL 1 goto err
+
 
 echo testing pass!
 exit 0
