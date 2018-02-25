ce85efcf8c4b674c30f95b8bb550404a9e855226
Artem Angaev <ololok@mail.ua>
2018-02-25 21:56:22 +0300

    Добавил тестов

--- Lab1/task2_flipbyte/test.bat
+++ Lab1/task2_flipbyte/test.bat
@@ -8,9 +8,17 @@ rem При запуске без параметров ожидается ненулевой код возврата
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
 
-rem При запуске с правильными параметрами ожидается нулевой код возврата
-rem %PROGRAM% test_data\input.txt Hello >nul
-rem if ERRORLEVEL 1 goto err
+rem При запуске с не числовым параметром ожидается не нулевой код возврата
+%PROGRAM% hello >nul
+if not ERRORLEVEL 1 goto err
+
+rem При запуске с числовым параметром ожидается не нулевой код возврата если число <0
+%PROGRAM% -1 
+if not ERRORLEVEL 1 goto err
+
+rem При запуске с числовым параметром ожидается не нулевой код возврата если число >255
+%PROGRAM% 256 
+if not ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами, но без возможности прочитать файл (не сущ) ожидает ненулевой код возврата
 rem %PROGRAM% notExistFile.txt Hello >nul
