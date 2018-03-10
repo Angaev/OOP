3927febb20f410c3a65dd34500c15a22f85fe971
Artem Angaev <ololok@mail.ua>
2018-03-10 09:48:58 +0300

    Добавлена функция CalcArithmeticSeries и тесты для нее

--- Catch_example/Catch_example/Catch_example.cpp
+++ Catch_example/Catch_example/Catch_example.cpp
@@ -12,12 +12,30 @@ unsigned int Factorial(unsigned int number) {
 //фибоначи по ее порядковому номеру
 unsigned int Fibonacci(unsigned int index)
 {
-	return 1;
+	return (index <= 1) ? 1 : Fibonacci(index - 1) + Fibonacci(index - 2);
+}
+
+int CalcArithmeticSeries(int a0, int commonDifference, int size)
+{
+	int sumArithmeticSeries = ((2 * a0 + commonDifference * (size - 1)) / 2) * size;
+	return sumArithmeticSeries;
+}
+//граничные: 0, 
+TEST_CASE("Arithmetic series are computed", "[ArithmeticSeries]")
+{
+	REQUIRE(CalcArithmeticSeries(1, 2, 0) == 0);
+	REQUIRE(CalcArithmeticSeries(1, 2, 1) == 1);
+	REQUIRE(CalcArithmeticSeries(58, 23, 23) == 7153);
+	REQUIRE(CalcArithmeticSeries(258, 78, 187) == 1404744);
 }
 
 TEST_CASE("Fibonacci numbers are computed", "[fibonacci]")
 {
 	REQUIRE(Fibonacci(0) == 1);
+	REQUIRE(Fibonacci(1) == 1);
+	REQUIRE(Fibonacci(2) == 2);
+	REQUIRE(Fibonacci(10) == 89);
+
 }
 
 TEST_CASE("Factorials are computed", "[factorial]") 
