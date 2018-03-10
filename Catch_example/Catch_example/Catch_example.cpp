0560960b8fb80c2cea2f9a2235d664a749c00ab4
Artem Angaev <ololok@mail.ua>
2018-03-10 09:23:48 +0300

    Стало лучше

---
+++ Catch_example/Catch_example/Catch_example.cpp
@@ -0,0 +1,30 @@
+﻿// Catch_example.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#define CATCH_CONFIG_MAIN
+#include "../../Catch/catch.hpp"
+
+unsigned int Factorial(unsigned int number) {
+	return number <= 1 ? 1: Factorial(number - 1)*number;
+}
+
+//фибоначи по ее порядковому номеру
+unsigned int Fibonacci(unsigned int index)
+{
+	return 1;
+}
+
+TEST_CASE("Fibonacci numbers are computed", "[fibonacci]")
+{
+	REQUIRE(Fibonacci(0) == 1);
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") 
+{
+	REQUIRE(Factorial(0) == 1);
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
+}
