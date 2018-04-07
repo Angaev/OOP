f2e7ec856daae29205e0b6c9069f3f11b01336da
Artem Angaev <ololok@mail.ua>
2018-04-07 17:08:56 +0300

    lab2 task2

--- Lab2/Task2/Task2/FindAndReplace.cpp
+++ Lab2/Task2/Task2/FindAndReplace.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "../Task2/FindAndReplace.h"
-#include <string>
+
 
 std::string FindAndReplace(std::string const& subject, std::string const& search, std::string const& replace)
 {
